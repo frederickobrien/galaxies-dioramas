04f55e0a2613ffd9f4257f3bc658ba928aa0352f
Frederick O'Brien <frederik.obrien@gmail.com>
2022-11-24 11:43:07 +0000

    Up and running

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Model as WebExperience } from "./assets/3D-Model/Web-experience"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <ambientLight intensity={1} />
+        <WebExperience />
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 }
